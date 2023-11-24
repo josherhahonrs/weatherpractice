688d3bee967266164954b05213d3059ae5c31829
joshuaerhahonrs <joshua.erhahon@riversafe.co.uk>
2023-11-24 13:26:46 +0000

    hello

---
+++ main.py
@@ -0,0 +1,26 @@
+import requests
+
+def weather():
+    headers = {
+        'X-RapidAPI-Key': "36d9867717msh152a633d26299dep192786jsnf80ec7e2e717",
+        'X-RapidAPI-Host': "weatherapi-com.p.rapidapi.com"
+    }
+    URL = "https://weatherapi-com.p.rapidapi.com/current.json?q="
+    user_input = input("Enter city: ")
+
+    data = requests.get(URL + user_input, headers=headers).json()
+
+    celsius_temp = data.get('current', {}).get('temp_c')
+    if celsius_temp is not None:
+        print(f"Temperature in {user_input.capitalize()}: {celsius_temp}°C")
+        return(200)
+    else:
+        print("Temperature data not available.")
+        return(404)
+
+
+
+Joshua = weather()
+print(Joshua)    
+
+
